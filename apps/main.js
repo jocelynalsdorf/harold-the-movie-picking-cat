760830a43b4898b02e77e18a0b870d91ad4d1c43
jocelynalsdorf <jfrodsla@gmail.com>
2014-12-06 08:48:36 +0700

    Initial commit due to problems with gh pages on movie-picker-api

---
+++ apps/main.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+var url = 'http://api.themoviedb.org/3/',
+    mode = 'discover/movie?',
+    year,
+    yearNum,
+    fav,
+    key = '&api_key=320a3fa0fbd7cfcae7cff15a4d705363';
+
+
+$('.user-info').submit(function (event) {
+          
+        event.preventDefault();
+
+        // get the value of the tags the user submitted
+        var year = $(this).find("input[name='element_1']").val();
+        
+        //changes string to integer 
+        var yearNum = parseInt(year);
+        var fav = $(this).find("input[name='element-radio']:checked").val();
+
+if(fav == 1) {
+            
+
+       $.ajax({
+            type: 'GET',
+            url: url + mode + "primary_release_year=" +yearNum+ "&vote_average.gte=7.0&vote_count.gte=3&" + key,
+            async: false,
+            jsonpCallback: 'results',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+                console.log(json);
+
+                var resultsArray = json.results;
+                var arrayLength = resultsArray.length;
+                for(var i = 0; i <arrayLength; i++) {
+                console.log(resultsArray[i].title, resultsArray[i].vote_average);
+                 $(".movie-title").append("<span> Movie Title: " + resultsArray[i].title + " Average rating: " +resultsArray[i].vote_average + "</span><br>"); 
+
+               }
+               
+            },
+
+            error: function(e) {
+                console.log(e.message);
+            }
+          
+
+  
+       });
+
+
+ }
+ else if(fav == 2) {
+        $.ajax({
+            type: 'GET',
+            url: url + mode + "primary_release_year=" +yearNum+ "&vote_average.lte=4.0&vote_count.gte=3&" + key,
+            async: false,
+            jsonpCallback: 'results',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+                console.log(json);
+                var resultsArray = json.results;
+                var arrayLength = resultsArray.length;
+                for(var i = 0; i <arrayLength; i++) {
+                console.log(resultsArray[i].title, resultsArray[i].vote_average);
+                $(".movie-title").append("<span> Movie Title: " + resultsArray[i].title + " Average rating: " +resultsArray[i].vote_average + "</span><br>"); 
+
+               }
+
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+            });
+            }
+
+
+
+
+    });
+
+$('.userreset').submit(function (event) {
+   event.preventDefault();
+      window.location.reload();
+
+});
+
+});
